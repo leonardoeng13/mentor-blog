2393683b8798cd95f5e3aefd931b825b6ec61e57
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2022-02-16 20:38:26 -0300

    Table column fix and remove and reference

--- db/migrate/20220216233155_create_likes.rb
+++ db/migrate/20220216233155_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20220216233215_create_comments.rb
+++ db/migrate/20220216233215_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
       t.text :text
 
